30d296484a198a420583a153f1d81e05ffa10bbc
SylvainBR <3095440-Sylvainn7@users.noreply.gitlab.com>
2021-02-01 16:51:35 +0100

    install routes

---
+++ src/Container/AppThreads.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class AppThreads extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello Fred
+            </div>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { AppMessage } from './Container/AppMessage';
-import { NavBar } from './navbar';
+import { AppThreads } from './Container/AppThreads';
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">New message</Link>
+            </li>
+            <li>
+              <Link to="/threads">Threads</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/threads">
+            <Threads />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h3>Welcome and write your new message !</h3>
+      <AppMessage />
+    </div>
+  )
+}
+
+function Threads() {
+  return (
+    <div>
+    <h3>List of Threads</h3>
+    <AppThreads />
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <div>
-    <NavBar />
-    <AppMessage />
-  </div>, 
+  <App />, 
 	document.getElementById('root')
 );
